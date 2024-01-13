dc64086075a1e86c84972c53ffb60b1c0f276dc9
arim83 <jorammercado@pursuit.org>
2024-01-12 19:19:32 -0500

    remove keys, update user controller

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -2,9 +2,9 @@ const express = require("express")
 const bcrypt = require("bcryptjs")
 
 const {
-  getOneUser,
-  getOneUserByEmail,
-  createUser,
+    getOneUser,
+    getOneUserByEmail,
+    createUser,
 } = require("../queries/users.js")
 // const{
 //   checkName,
@@ -16,16 +16,51 @@ const users = express.Router()
 
 // LOGIN ROUTE
 users.post("/login", async (req, res) => {
-
+    const oneUser = await getOneUserByEmail(req.body)
+    if (oneUser) {
+        bcrypt.compare(req.body.password, oneUser.password).then((isMatch) => {
+            if (isMatch) {
+                res.status(200).json(oneUser)
+            }
+            else {
+                res.status(400).json({ error: "incorect password" })
+            }
+        })
+    }
+    else {
+        res.status(404).json({ error: "not found!" })
+    }
 })
 
 // SIGN UP ROUTE
 users.post("/", async (req, res) => {
-    
+    const registeredUser = await getOneUserByEmail(req.body)
+    if(registeredUser)
+        return res.status(400).json({error: "user already registered with this address"})
+    else{
+        const newUser = req.body
+        bcrypt.genSalt(10, async(err, salt) => {
+            bcrypt.hash(newUser.password, salt, async (err, hash) => {
+                if(err) throw err
+                console.log(hash, " hash ")
+                newUser.password = hash
+                try{
+                    const createdUser = await createdUser(newUser)
+                    console.log(createdUser, " created user here")
+                    if(createdUser.id){
+                        res.status(200).json(createdUser)
+                    }
+                }
+                catch(error){
+                    res.status(400).json({error: "error creating user"})
+                }
+            })
+        })
+    }
 })
 
 
 
-    
+
 
 module.exports = users;
