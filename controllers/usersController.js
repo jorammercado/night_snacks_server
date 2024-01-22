870434b846affbbb3eac58ae33e762b6255fa079
arim83 <jorammercado@pursuit.org>
2024-01-21 23:53:29 -0500

    handle default profile image on frontend

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -54,10 +54,7 @@ users.post("/", checkName, checkEmail, checkPassword, async (req, res) => {
                 if (err) throw err
                 newUser.password = hash
                 try {
-                    newUser.profile_img = !newUser.profile_img ? `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-file-person" viewBox="0 0 16 16">
-                    <path d="M12 1a1 1 0 0 1 1 1v10.755S12 11 8 11s-5 1.755-5 1.755V2a1 1 0 0 1 1-1zM4 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2z"/>
-                    <path d="M8 10a3 3 0 1 0 0-6 3 3 0 0 0 0 6"/>
-                    </svg>` : newUser.profile_img
+                    newUser.profile_img = !newUser.profile_img ? "profile image" : newUser.profile_img
                     newUser.firstname = !newUser.firstname ? "unknown first name" : newUser.firstname
                     newUser.lastname = !newUser.lastname ? "unknown last name" : newUser.lastname
                     newUser.about = !newUser.about ? "about me" : newUser.about
